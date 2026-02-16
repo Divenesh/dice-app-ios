690d2b588f4e3619db128cd2c9bfe5343c8cec12
Divenesh Shamugam <77532370+Divenesh@users.noreply.github.com>
2026-02-16 18:45:58 +0800

    feat: added constraints

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -1,19 +1,32 @@
 //
 //  ViewController.swift
-//  Dice
+//  Dicee-iOS13
 //
-//  Created by Divenesh Shamugam on 16/02/2026.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        let diceName = ["One","Two","Three","Four","Five","Six"]
+        let random1 = Int.random(in: 0...5)
+        let random2 = Int.random(in: 0...5)
+        
+        diceImage1.image = UIImage(named: "Dice" + diceName[random1])
+        diceImage2.image = UIImage(named: "Dice" + diceName[random2])
+    }
+    
 }
 
