40aeb10151950818e84e87e0c8739d21b7e99867
Divenesh Shamugam <77532370+Divenesh@users.noreply.github.com>
2026-02-16 18:23:29 +0800

    Initial Commit

---
+++ DiceTests/DiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceTests.swift
+//  DiceTests
+//
+//  Created by Divenesh Shamugam on 16/02/2026.
+//
+
+import Testing
+@testable import Dice
+
+struct DiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
